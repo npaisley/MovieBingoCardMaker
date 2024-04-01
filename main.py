6f2a28637e6a134d0d0b7c37a3b259f0de0196a1
npaisley <47052925+npaisley@users.noreply.github.com>
2024-04-01 15:06:36 +0200

    update

--- main.py
+++ main.py
@@ -1,44 +1,72 @@
 import random
-import csv
-from weasyprint import HTML
-from pathlib import Path
-import sys
+from jinja2 import Environment, FileSystemLoader
 
 
-def read_csvs(csv_folder: Path, movie_name: str = None) -> list:
+# TODO getopts to set parameters
+# TODO menu for selecting movie (set by csv title)
+# TODO read quotes from csv files (seperate for center (freebie), reg cliches, movie specific)
+# TODO options for amount of movie vs cliches
+# TODO options for how many bingo cards
+# TODO set title depending on movie
+def generate_bingo_card():
+    quotes = [
+        "I'm trying to protect you.",
+        "At least you have a home.",
+        "Or die trying.",
+        "Don't you die on me.",
+        "Did I just say that out loud?",
+        "You just don't get it.",
+        "The only thing I don't regret is you.",
+        "I'm in.",
+        "NOOOOOOOOOOOOOOOO!",
+        '"We only have one shot." or "Make it count."',
+        "You're/we're no match for me/them.",
+        "It was personal to me!",
+        "Kill me.",
+        "Only one person/thing can do/stop X.",
+        "I've seen this in a dream.",
+        "It's not what it looks like!",
+        "You'll know it when you see it.",
+        "Do you know who I am?",
+        '"Don\'t laugh." tells story, they laugh',
+        '"I won\'t do X." immediately does X',
+        "Can I/you afford X?" "I/you can't afford not to X.",
+        "We've got company!",
+        "It's showtime.",
+        "It's right behind me, isn't it?",
+        '"I\'ve been expecting you." or "I knew you\'d come."',
+        "There's no time!"
+    ]
+
+    # Randomly shuffle the quotes
+    random.shuffle(quotes)
+
+    # Create a 5x5 grid with quotes
+    card = [quotes[i:i+5] for i in range(0, 25, 5)]
+
+    return card
+
+
+def load_quotes_csv():
     """
-    reads general cliche csv and if a specific movie is specified that one too
-    returns a list of cliches
-    also needs to return a list of center bingo options
-    maybe a dict of the two for ease
-    :param csv_folder:
-    :param movie_name:
+    loads cliches, freebies, and movie specifics quotes
     :return:
     """
-    # TODO
     pass
 
 
-def bingo_quotes(quotes: dict) -> dict:
-    # TODO take list of all quotes and return list of 24 random ones and one center option
-    pass
+def draw_bingo_card_html(card):
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('/assets/card.html')
 
+    html_content = template.render(card=card)
 
-def card_html():
-    # TODO use html template and quotes to make a html for the specific bingo card
-    pass
+    with open("bingo_card.html", "w", encoding="utf-8") as html_file:
+        html_file.write(html_content)
 
-
-def card_html_to_pdf():
-    # TODO read card html and convert all to pdfs
-    pass
-
-
-def main():
-    # TODO parse cmd line arguments for movie csv name, number of bingo cards required
-    # TODO make bingo cards
-    pass
+    print("Bingo card saved as bingo_card.html")
 
 
 if __name__ == "__main__":
-    main()
+    card = generate_bingo_card()
+    draw_bingo_card_html(card)
